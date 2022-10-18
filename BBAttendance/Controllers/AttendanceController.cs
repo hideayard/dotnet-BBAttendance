647e07aa66004e6dcdfe63752a5cc99a485763c1
hideayard <hideayard@gmail.com>
2022-10-18 11:21:08 +0700

    attendance

---
+++ BBAttendance/Controllers/AttendanceController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BBAttendance.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AttendanceController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public AttendanceController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Attendance>>> Get()
+        {
+            return Ok(await _context.Attendances.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Attendance>> Get(int id)
+        {
+            var data = await _context.Attendances.FindAsync(id);
+            if (data == null)
+                return BadRequest("Attendance not found.");
+            return Ok(data);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Attendance>>> Attend(Attendance data)
+        {
+            _context.Attendances.Add(data);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Attendances.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Attendance>>> Delete(int id)
+        {
+            var dbAttendance = await _context.Attendances.FindAsync(id);
+            if (dbAttendance == null)
+                return BadRequest("Attendance not found.");
+
+            _context.Attendances.Remove(dbAttendance);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Attendances.ToListAsync());
+        }
+
+    }
+}
---
+++ BBAttendance/Migrations/20221017105818_UpdateTableUsers.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BBAttendance.Migrations
+{
+    public partial class UpdateTableUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Attendance_Users_UserId",
+                table: "Attendance");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Attendance",
+                table: "Attendance");
+
+            migrationBuilder.RenameTable(
+                name: "Attendance",
+                newName: "Attendances");
+
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Users",
+                newName: "Username");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Attendance_UserId",
+                table: "Attendances",
+                newName: "IX_Attendances_UserId");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<string>(
+                name: "RefreshToken",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TokenCreated",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "TokenExpires",
+                table: "Users",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Attendances",
+                table: "Attendances",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Attendances_Users_UserId",
+                table: "Attendances",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Attendances_Users_UserId",
+                table: "Attendances");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Attendances",
+                table: "Attendances");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RefreshToken",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TokenCreated",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TokenExpires",
+                table: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "Attendances",
+                newName: "Attendance");
+
+            migrationBuilder.RenameColumn(
+                name: "Username",
+                table: "Users",
+                newName: "Password");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Attendances_UserId",
+                table: "Attendance",
+                newName: "IX_Attendance_UserId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Attendance",
+                table: "Attendance",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Attendance_Users_UserId",
+                table: "Attendance",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
